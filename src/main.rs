d5a8762749a4e42191ab85c864eaf1e5077db7d0
naoya7076 <naoya7076@gmail.com>
2025-01-19 22:46:50 +0900

    再帰構造の実装

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use serde_json::json;
+use crate::RecursiveEnum::{Null, Val};
+
+#[derive(Debug)]
+enum RecursiveEnum {
+    Val(Box<RecursiveEnum>),
+    Null,
+}
 
 fn main() {
-    let hanako = json!({
-        "name": "Hanako",
-        "age": 25,
-        "favorites": {
-            "food": ["Sushi", "Ramen"],
-        }
-    });
-    println!("{:?}", hanako);
+    let val = Val(Box::new(Val(Box::new(Val(Box::new(Null))))));
+    println!("{:?}", val);
 }
