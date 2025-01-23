54ea6f7e7e0f7cb526b51252950307b0426bc178
naoya7076 <naoya7076@gmail.com>
2025-01-23 23:05:25 +0900

    BufReaderを使って1行ずつ読み出す

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
 use std::fs::File;
-use std::io::Read;
 
-const BUFSIZE: usize = 1024;
-
+mod line_read;
 fn main() -> std::io::Result<()> {
-    let mut f = File::open("hello.txt")?;
-    let mut buf = [0_u8; BUFSIZE];
+    let f = File::open("hello.txt")?;
 
-    let mut lines = Vec::new();
-    let mut linebuf = String::new();
+    let mut lines_vec = line_read::read_lines(f);
 
-    loop {
-        let read_size = f.read(&mut buf)?;
-        if read_size == 0 {
-            break;
-        }
-
-        for cc in &buf[..read_size] {
-            if *cc == b'\n' {
-                lines.push(linebuf);
-                linebuf = String::new();
-            } else {
-                linebuf.push(*cc as char);
-            }
-        }
-    }
-    println!("{:?}", lines);
+    println!("{:?}", lines_vec);
     Ok(())
 }
