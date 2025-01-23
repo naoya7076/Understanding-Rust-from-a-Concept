2a3d8ee2b05f7b16d25dba7b815d5d876a1a37d2
naoya7076 <naoya7076@gmail.com>
2025-01-23 23:07:45 +0900

    mut to immutable

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod line_read;
 fn main() -> std::io::Result<()> {
     let f = File::open("hello.txt")?;
 
-    let mut lines_vec = line_read::read_lines(f);
+    let lines_vec = line_read::read_lines(f);
 
     println!("{:?}", lines_vec);
     Ok(())
