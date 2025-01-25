9f2be9b2f449d342a5badcc4c8b3da23fe69b523
naoya7076 <naoya7076@gmail.com>
2025-01-25 23:27:10 +0900

    スレッドによる並列実行

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use std::fs::File;
+const N_MAX: usize = 100000000;
+const N_THREADS: usize = 4;
 
-mod line_read;
-fn main() -> std::io::Result<()> {
-    let f = File::open("hello.txt")?;
+const N_ELEM_PER_THREAD: usize = N_MAX / N_THREADS;
+const RESIDUAL: usize = N_MAX % N_THREADS;
 
-    let lines_vec = line_read::read_lines(f);
+fn main() -> std::thread::Result<()> {
+    if RESIDUAL != 0 {
+        panic!("N_MAX must be divisible by N_THREADS");
+    }
 
-    println!("{:?}", lines_vec);
+    let mut thread = Vec::new();
+    let v = std::sync::Arc::new((1..=N_MAX).collect::<Vec<usize>>());
+
+    for i in 0..N_THREADS {
+        let start = i * N_ELEM_PER_THREAD;
+        let end = start + N_ELEM_PER_THREAD;
+        let vv = std::sync::Arc::clone(&v);
+        let th = std::thread::spawn(move || vv[start..end].iter().sum::<usize>());
+        thread.push(th);
+    }
+
+    let ans: usize = thread.into_iter().map(|r| r.join().unwrap()).sum::<usize>();
+    println!("ans: {}", ans);
+    assert_eq!(ans, N_MAX * (N_MAX + 1) / 2);
     Ok(())
 }
