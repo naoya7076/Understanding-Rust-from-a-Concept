82baec3138c0e7ed9645858c798af1da2cbbd552
naoya7076 <naoya7076@gmail.com>
2025-01-26 14:52:58 +0900

    スレッド間での共有データに書き込み

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
-const N_MAX: usize = 100000000;
-const N_THREADS: usize = 4;
+use std::sync::{Arc, Mutex};
+use std::thread::spawn;
 
-const N_ELEM_PER_THREAD: usize = N_MAX / N_THREADS;
-const RESIDUAL: usize = N_MAX % N_THREADS;
+fn main() {
+    let data = Arc::new(Mutex::new(Vec::new()));
 
-fn main() -> std::thread::Result<()> {
-    if RESIDUAL != 0 {
-        panic!("N_MAX must be divisible by N_THREADS");
-    }
+    let added = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     let mut thread = Vec::new();
-    let v = std::sync::Arc::new((1..=N_MAX).collect::<Vec<usize>>());
-
-    for i in 0..N_THREADS {
-        let start = i * N_ELEM_PER_THREAD;
-        let end = start + N_ELEM_PER_THREAD;
-        let vv = std::sync::Arc::clone(&v);
-        let th = std::thread::spawn(move || vv[start..end].iter().sum::<usize>());
+    for a in added {
+        let data = Arc::clone(&data);
+        let th = spawn(move || {
+            let mut data = data.lock().unwrap();
+            data.push(a);
+        });
         thread.push(th);
     }
+    thread.into_iter().for_each(|th| {
+        let _ = th.join();
+    });
 
-    let ans: usize = thread.into_iter().map(|r| r.join().unwrap()).sum::<usize>();
-    println!("ans: {}", ans);
-    assert_eq!(ans, N_MAX * (N_MAX + 1) / 2);
-    Ok(())
+    let x = data.lock().unwrap();
+    println!("{:?}", x);
 }
