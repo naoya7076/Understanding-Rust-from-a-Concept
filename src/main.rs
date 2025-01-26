6303c6a8e8d421d4188e12fbdba9f2f810600a17
naoya7076 <naoya7076@gmail.com>
2025-01-26 16:24:31 +0900

    ソケットの入出力を使ったサーバ

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-use std::{sync::mpsc::channel, thread::spawn};
+use std::io::Read;
+use std::net::TcpListener;
+use std::{error::Error, io::Write, net::TcpStream};
+
+fn echo_process(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut buf = [0_u8; 1024];
+    loop {
+        stream.write_all("input =>".as_bytes())?;
+        let read = stream.read(&mut buf);
+        match read {
+            Ok(0) => break,
+            Ok(n) => {
+                stream.write_all("output=>".as_bytes())?;
+                stream.write_all(&buf[..n])?;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
+    }
+    Ok(())
+}
 
 fn main() {
-    let data = vec![1, 2, 3, 4, 5];
-    let (tx, rx) = channel::<i32>();
-
-    let data_len = data.len();
-
-    for d in data {
-        let tx = tx.clone();
-        spawn(move || tx.send(d));
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("port number is required");
     }
+    let port: usize = args[1].parse().expect("port number must be a number");
+    let addr = format!("localhost:{}", port);
+    let listener = TcpListener::bind(addr).unwrap();
+    println!("listening on {}", port);
 
-    for _ in 0..data_len {
-        println!("{}", rx.recv().unwrap());
+    loop {
+        let (mut stream, _) = listener.accept().unwrap();
+        std::thread::spawn(move || {
+            echo_process(&mut stream).unwrap();
+        });
     }
 }
