190c9e3a93815ae61d6fa210e33cde8f9e8214f2
naoya7076 <naoya7076@gmail.com>
2025-01-26 22:01:26 +0900

    非同期タスクのtokioランタイムによる実行

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,17 @@
-use std::io::Read;
-use std::net::TcpListener;
-use std::{error::Error, io::Write, net::TcpStream};
-
-fn echo_process(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
-    let mut buf = [0_u8; 1024];
-    loop {
-        stream.write_all("input =>".as_bytes())?;
-        let read = stream.read(&mut buf);
-        match read {
-            Ok(0) => break,
-            Ok(n) => {
-                stream.write_all("output=>".as_bytes())?;
-                stream.write_all(&buf[..n])?;
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
-        }
-    }
-    Ok(())
+async fn sum_func(n: usize) -> usize {
+    let ans = (1..=n).sum::<usize>();
+    println!("sum: {}", ans);
+    ans
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        panic!("port number is required");
-    }
-    let port: usize = args[1].parse().expect("port number must be a number");
-    let addr = format!("localhost:{}", port);
-    let listener = TcpListener::bind(addr).unwrap();
-    println!("listening on {}", port);
-
-    loop {
-        let (mut stream, _) = listener.accept().unwrap();
-        std::thread::spawn(move || {
-            echo_process(&mut stream).unwrap();
-        });
-    }
+    let ls = tokio::task::LocalSet::new();
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    ls.block_on(&rt, async {
+        sum_func(10000000).await;
+        sum_func(20000000).await;
+    });
 }
